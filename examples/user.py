6b66849f177e2bf28aa0dee104433d755a839bf7
aragorn-wu <wuqingfeng234@163.com>
2017-11-29 14:03:18 +0800

    增加用户密码修改的功能

---
+++ examples/user.py
@@ -0,0 +1,17 @@
+from onvif import ONVIFCamera
+
+if __name__ == '__main__':
+    mycam = ONVIFCamera('10.10.16.19', 80, 'admin', 'admin1234')
+    devicemgmt_service = mycam.create_devicemgmt_service()
+
+    users = devicemgmt_service.GetUsers()
+    for user in users:
+        if user.Username=="admin":
+            user.Password="admin123"
+
+
+    request = devicemgmt_service.create_type("SetUser")
+    request.User = users
+    request.ForcePersistence = False
+
+    devicemgmt_service.SetUser(request)
