0f0a067b86a0c217571738628f6931239dc0cd57
aragorn-wu <wuqingfeng234@163.com>
2017-12-14 17:04:40 +0800

    添加音频相关代码

--- examples/media.py
+++ examples/media.py
@@ -1,8 +1,20 @@
 from onvif import ONVIFCamera
 
 
-class video(object):
+class Media(object):
+    def __init__(self, ip, port, administratorName, administratorPassword):
+        self.ip = ip
+        self.port = port
+        self.administratorName = administratorName
+        self.administratorPassword = administratorPassword
+
     def getVideoInfo(self):
         mycam = ONVIFCamera(self.ip, self.port, self.administratorName, self.administratorPassword)
         media_service = mycam.create_media_service(False)
-        media_service.
+        ret = media_service.GetAudioSources()
+        print(ret)
+
+
+if __name__ == '__main__':
+    media_service = Media('10.10.17.18', 80, 'admin', 'admin123')
+    media_service.getVideoInfo()
